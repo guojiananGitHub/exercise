e743d9d11c04d9fc867eb88658039fc7c598d037
guojianan <guo.j.an@163.com>
2018-01-19 15:03:03 +0800

    根據ip定位準確地址信息

---
+++ ip.py
@@ -0,0 +1,31 @@
+import sys
+from urllib import request
+import json
+
+
+def get_ip_information(ip):
+    url = 'http://api.map.baidu.com/location/ip?ip=' + ip + '&ak=c8XPM8PdNP2pwsvPgeabelPDuLuTcSN0&coor=bd09ll'
+    poiss = ''
+    with request.urlopen(url) as f:
+        data_json = f.read()
+        data_dic = json.loads(data_json)
+        print(data_dic)
+#   百度api已改，注釋代碼不適用
+#         if data_dic():
+#             content = data_dic["content"]
+#             address_component = content["address_component"]
+#             formatted_address = content["formatted_address"]
+#             print("该IP地址的具体位置为：")
+#             print(address_component["country"])
+#             print(formatted_address)
+#             if content.key("pois"):
+#                 print("该IP地址附近POI信息如下：")
+#                 pois = content["pois"]
+#                 for index in range(len(pois)):
+#                     pois_name = pois[index]["name"]
+#                     pois_address = pois[index]["address"]
+#                     print(pois_name, pois_address)
+#             else:
+#                 print('IP地址定位失败！！！')
+if __name__ == '__main__':
+    get_ip_information('125.89.5.196')
