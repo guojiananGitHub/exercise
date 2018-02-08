be259c0731347b3305698ab8851f02992549238b
guojianan <guo.j.an@163.com>
2018-02-08 19:38:33 +0800

    排序

--- do_sorted.py
+++ do_sorted.py
@@ -1,1 +1,9 @@
 print(sorted([36, 5, -12, 9, -21]))
+
+print(sorted([36, 5, -12, 9, -21], key = abs))
+
+print(sorted(['bob', 'about', 'Zoo', 'Credit']))
+
+print(sorted(['bob', 'about', 'Zoo', 'Credit'], key=str.lower)) # 排序忽略大小写，按照字母序排序
+
+print(sorted(['bob', 'about', 'Zoo', 'Credit'], key=str.lower, reverse=True)) # 反向排序
