879480b7a842dbc77b0a1f5517260d2d5dd9046a
guojianan <guo.j.an@163.com>
2018-02-14 15:53:01 +0800

    urllib

---
+++ use_urllib.py
@@ -0,0 +1,53 @@
+import urllib
+from urllib import request, parse
+
+# 对豆瓣的一个URLhttps://api.douban.com/v2/book/2129650进行抓取，并返回响应
+# with request.urlopen('https://api.douban.com/v2/book/2129650') as f:
+#     data = f.read()
+#     print('Status:', f.status, f.reason)
+#     for k, v in f.getheaders():
+#         print('%s: %s ' % (k, v))
+#     print('Data:', data.decode('utf-8'))
+
+# 模拟iPhone 6去请求豆瓣首页：
+# req = request.Request('http://www.douban.com/')
+# req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+# with request.urlopen(req) as f:
+#     print('Status:', f.status, f.reason)
+#     for k, v in f.getheaders():
+#         print('%s: %s ' % (k, v))
+#     print('Data:', f.read().decode('utf-8'))
+
+# 模拟一个微博登录，先读取登录的邮箱和口令，然后按照weibo.cn的登录页的格式以username=xxx&password=xxx的编码传入：
+# print('Login to weibo.cn...')
+# email = input('Email: ')
+# passwd = input('Password: ')
+# login_data = parse.urlencode([
+#     ('username', email),
+#     ('password', passwd),
+#     ('entry', 'mweibo'),
+#     ('client_id', ''),
+#     ('savestate', '1'),
+#     ('ec', '',),
+#     ('pagerefer', 'https://passport.weibo.cn/signin/welcome?entry=mweibo&r=http%3A%2F%2Fm.weibo.cn%2F')
+# ])
+#
+# req = request.Request('https://passport.weibo.cn/sso/login')
+# req.add_header('Origin', 'https://passport.weibo.cn')
+# req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+# req.add_header('Referer', 'https://passport.weibo.cn/signin/login?entry=mweibo&res=wel&wm=3349&r=http%3A%2F%2Fm.weibo.cn%2F')
+#
+# with request.urlopen(req, data=login_data.encode('utf-8')) as f:
+#     print('Status:', f.status, f.reason)
+#     for k, v in f.getheaders():
+#         print('%s: %s' % (k, v))
+#     print('Data:', f.read().decode('utf-8'))
+
+# 通过一个Proxy去访问网站，我们需要利用ProxyHandler来处理
+proxy_handler = urllib.request.ProxyHandler({'http': 'https://www.google.com.hk/'})
+proxy_auth_handler = urllib.request.ProxyBasicAuthHandler()
+proxy_auth_handler.add_password('realm', 'host', 'username', 'password')
+opener = urllib.request.build_opener(proxy_handler, proxy_auth_handler)
+with opener.open('https://accounts.google.com/signin') as f:
+    pass
+
