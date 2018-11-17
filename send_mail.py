7db47bb031a0b998440af0f286dfa49a8a83524d
guojianan <guo.j.an@163.com>
2018-11-17 18:03:20 +0800

    mail

--- send_mail.py
+++ send_mail.py
@@ -1,29 +1,50 @@
+from email.mime.text import MIMEText
+import smtplib
 from email import encoders
 from email.header import Header
-from email.mime.text import MIMEText
 from email.utils import parseaddr, formataddr
-
-import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
 
 
 def _format_addr(s):
     name, addr = parseaddr(s)
     return formataddr((Header(name, 'utf-8').encode(), addr))
 
-from_addr = input('From: ')
-password = input('Password: ')
-to_addr = input('To: ')
-smtp_server = input('SMTP server: ')
 
-msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+# 输入email地址和口令
+from_addr = input('From（QQ邮箱）:')
+password = input('Password(授权码):')
+# 输入收件人地址：
+to_addr = input('To:')
+# 输入SMTP服务器地址：
+smtp_server = input('SMTP server:')
+
+# 邮件正文
+msg = MIMEMultipart()
+# msg = MIMEText('真不知道说啥', 'plain', 'utf-8')  # 发送纯文本
+# msg = MIMEText('<html><body><h1>Hello</h1>' +
+#     '<p>send by <a href="http://www.python.org">Python</a>...</p>' +
+#     '</body></html>', 'html', 'utf-8')  # 发送html
 msg['From'] = _format_addr('Python爱好者 <%s>' % from_addr)
 msg['To'] = _format_addr('管理员 <%s>' % to_addr)
 msg['Subject'] = Header('来自SMTP的问候……', 'utf-8').encode()
 
-server = smtplib.SMTP_SSL(smtp_server, 465)
+# 邮件正文是MIMEText:
+msg.attach(MIMEText('send with file...', 'plain', 'utf-8'))
+
+# 添加附件就是加上一个MIMEBase，从本地读取一个图片
+with open('C:\\Users\\administrator\Desktop\\QQ20181112103228.png', 'rb') as f:
+    mime = MIMEBase('image', 'png', filename='QQ20181112103228.png')
+    mime.add_header('Content-Disposition', 'attachment', filename='QQ20181112103228.png')
+    mime.add_header('Content-ID', '<0>')
+    mime.add_header('X-Attachment-Id', '0')
+    mime.set_payload(f.read())
+    encoders.encode_base64(mime)
+    msg.attach(mime)
+
+server = smtplib.SMTP(smtp_server, 25)  # SMTP协议默认端口是25
 server.set_debuglevel(1)
 server.login(from_addr, password)
 server.sendmail(from_addr, [to_addr], msg.as_string())
 server.quit()
-
-# qkjerduknvjxbajd
