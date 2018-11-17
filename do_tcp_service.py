e9f612a8fe0c10eeb94d10b820ee4cd91837ed63
guojianan <guo.j.an@163.com>
2018-11-17 11:14:49 +0800

    TCP

--- do_tcp_service.py
+++ do_tcp_service.py
@@ -2,20 +2,14 @@ import socket
 import threading
 import time
 
+
 # 创建一个socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # 监听端口
-s.bind(('127.0.0.1', 9999))
+s.bind(('127.0.0.1', 5000))
 s.listen(5)
 print('Waiting for connection...')
-
-while True:
-    # 接受一个新连接
-    sock, addr = s.accept()
-    # 创建新线程来处理TCP连接:
-    t = threading.Thread(target=tcplink, args=(sock, addr))
-    t.start()
 
 
 def tcplink(sock, addr):
@@ -26,6 +20,14 @@ def tcplink(sock, addr):
         time.sleep(1)
         if not data or data.decode('utf-8') == 'exit':
             break
-        sock.send(('Hello, %s' % data.decode('utf-8')).encode('utf-8'))
+        sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
     sock.close()
     print('Connection from %s:%s closed.' % addr)
+
+
+while True:
+    # 接受一个新连接:
+    sock, addr = s.accept()
+    # 创建新线程来处理TCP连接:
+    t = threading.Thread(target=tcplink, args=(sock, addr))
+    t.start()
