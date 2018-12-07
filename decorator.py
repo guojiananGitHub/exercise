8b3e22ed7981e2ff14da40323c0a93a0547b0dd4
guojianan <guo.j.an@163.com>
2018-12-07 18:02:12 +0800

    装饰器

--- decorator.py
+++ decorator.py
@@ -1,6 +1,26 @@
+import functools
+
+
+def log(text):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            print('%s %s():' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+
+# 函数也是一个对象
+@log('execute')
 def now():
     print('2015-3-25')
-f = now
-# print(f())
-print(now.__name__)
-print(f.__name__)
+
+
+# 函数对象可以被赋值给变量
+# f = now
+# # print(f()) # 调用函数对象
+# print(now.__name__)  # __name__属性可以拿到函数的名字
+# print(f.__name__)
+
+print(now())
